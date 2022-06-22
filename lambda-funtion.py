4e0457abee098625c5b00342a9da80f4647b6633
[first Lastname] <[valid-email]>
2022-06-22 17:59:12 +0530

    second commit

---
+++ lambda-funtion.py
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!  !....... (Modified v0.2'),
+    };
+    return response;
+};
